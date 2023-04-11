ab57b99f48e9cd7a13d68fee1763739ff736b267
Matt Zhao <mattzhao@MP.ht.home>
2023-04-11 14:44:27 -0300

    app code

---
+++ server.js
@@ -0,0 +1,89 @@
+// import required modules and initializes constants, variables and the Express application.
+const express = require('express')
+const fs = require('fs')
+
+const { readFromFile, readAndAppend } = require('./helpers/file')
+const uuid = require('./helpers/uuid');
+const db = require('./db/db.json');
+// const { log } = require('console');
+// const { stringify } = require('querystring');
+
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+//sets up a route to serve the "notes.html" file when the "/notes" path is requested via a GET request
+app.get('/notes', (req, res)=>{
+    res.sendFile(`${__dirname}/public/notes.html`)  
+})
+
+// sets up a route to retrieve and return all notes from the database file "/db/db.json" when a GET request is made to "/api/notes" path. 
+//uses the helper function readFromFile to read the contents of the database file and parse it to a JSON object to be returned as a response.
+app.get('/api/notes', (req, res)=>{
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+})
+
+  // readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+
+app.post('/api/notes', (req, res)=>{
+    console.info(`${req.method} request received to add a new note`)
+    const { title, text } = req.body;
+
+    // If all the required properties are present
+    if (title && text ) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id:uuid(),
+      }
+
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      }
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting a new note');
+    }
+
+})
+
+app.delete(`/api/notes/:id`, (req, res)=>{
+    const id = req.params.id;
+    const noteToDelete = db.find(el => el.id === id);
+    if (!noteToDelete) {
+       return res.status(404).json({ error: 'Note not found' });
+    }
+    const filteredNotes = db.filter(note => note.id !== id);
+
+    fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(filteredNotes, null, 4), (err)=>{
+       if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to delete note' });
+       }
+       console.log(`Note with ID ${id} deleted`);
+       res.json({ message: 'Note deleted successfully' });
+    });
+ })
+ 
+
+app.get('*', (req, res)=>{
+    res.sendFile(`${__dirname}/public/index.html`)
+    
+})
+app.listen(PORT, ()=>
+{
+    console.log(`Sever started, listening on PORT: ${PORT}!`);
+})
